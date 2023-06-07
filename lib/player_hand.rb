93ea1d6f0f7576d78b6c66bb184f983a8781d479
Toby Muir-Little <tjcmui@gmail.com>
2023-06-07 16:21:43 +0100

    Expand player hand to include all types

--- lib/player_hand.rb
+++ lib/player_hand.rb
@@ -7,10 +7,28 @@ class PlayerHand
   end
 
   def value
-    first_card + second_card
+    convert_card_to_value(first_card) + convert_card_to_value(second_card)
   end
 
   def type
+    return :split if hand_split?
+    return :soft if card_is_ace?(first_card) || card_is_ace?(second_card)
+
     :hard
+  end
+
+  def convert_card_to_value(card)
+    return 1 if card_is_ace?(card) && hand_split?
+    return 11 if card_is_ace?(card)
+
+    card
+  end
+
+  def card_is_ace?(card)
+    card == 'A'
+  end
+
+  def hand_split?
+    first_card == second_card
   end
 end
--- spec/player_hand_spec.rb
+++ spec/player_hand_spec.rb
@@ -18,11 +18,39 @@ RSpec.describe PlayerHand do
     it 'returns the value of the hand' do
       expect(player_hand.new(first_card: 2, second_card: 3).value).to eq(5)
     end
+
+    it 'returns the value of the hand if split' do
+      expect(player_hand.new(first_card: 3, second_card: 3).value).to eq(6)
+    end
+
+    it 'returns the value of the hand using ace as 11 if soft' do
+      expect(player_hand.new(first_card: 'A', second_card: 3).value).to eq(14)
+    end
+
+    it 'returns the value of the hand using ace as 11' do
+      expect(player_hand.new(first_card: 'A', second_card: 3).value).to eq(14)
+    end
+
+    it 'returns the value of the hand as 2 if two aces' do
+      expect(player_hand.new(first_card: 'A', second_card: 'A').value).to eq(2)
+    end
   end
 
   describe '.type' do
     it 'returns the type of hand' do
       expect(player_hand.new(first_card: 2, second_card: 3).type).to eq(:hard)
+    end
+
+    it 'returns the type of hand as soft if ace included' do
+      expect(player_hand.new(first_card: 'A', second_card: 3).type).to eq(:soft)
+    end
+
+    it 'returns the type of hand as split if the same cards included' do
+      expect(player_hand.new(first_card: 3, second_card: 3).type).to eq(:split)
+    end
+
+    it 'returns the type of hand as split if the same cards included' do
+      expect(player_hand.new(first_card: 'A', second_card: 'A').type).to eq(:split)
     end
   end
 end
