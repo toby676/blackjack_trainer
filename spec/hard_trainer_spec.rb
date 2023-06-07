63a7db4c930ba2ace4c5710a522076e4a4b0097b
Toby Muir-Little <tjcmui@gmail.com>
2023-06-07 14:16:31 +0100

    Check hit for dealer 2

--- spec/hard_trainer_spec.rb
+++ spec/hard_trainer_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe HardTrainer do
   context 'dealer has 2' do
     subject(:dealer) { HardTrainer.new(dealer: 2) }
 
-    it 'returns hit if player has 4' do
-      expect(dealer.call(player:4)).to eq(:hit)
+    player_hands = (4..8).to_a + [9,12]
+
+    player_hands.each do |hand|
+      it "returns hit if player has #{hand}" do
+        expect(dealer.call(player: hand)).to eq(:hit)
+      end
     end
   end
 end
