9c4aebea761647c0d5f1582d3c592ba1e30a795b
Toby Muir-Little <tjcmui@gmail.com>
2023-06-07 14:52:20 +0100

    Handle all dealer = 3 hands

--- lib/hard_trainer.rb
+++ lib/hard_trainer.rb
@@ -4,15 +4,23 @@ class HardTrainer
   end
 
   def call(player_total:)
-    player_hit_hands = (4..8).to_a + [9,12]
-    player_double_hands = [10,11]
+    move_hash = matrix.detect{_1[:dealer].include?(dealer_card)}
+    return :unknown unless move_hash
 
-    return :hit if player_hit_hands.include?(player_total)
-    return :double if player_double_hands.include?(player_total)
-    :stand
+    move_hash.select{|_,v| v.include?(player_total)}.keys.first
   end
 
   private
-
   attr_reader :dealer_card
+
+  def matrix
+    [
+      {
+        dealer: [2], hit: (4..8).to_a + [9,12], double: [10,11], stand: (13..20)
+      },
+      {
+        dealer: [3], hit: (4..8).to_a + [12], double: (9..11), stand: (13..20)
+      }
+    ]
+  end
 end
--- spec/hard_trainer_spec.rb
+++ spec/hard_trainer_spec.rb
@@ -25,4 +25,29 @@ RSpec.describe HardTrainer do
       end
     end
   end
+
+  context 'dealer has 3' do
+    subject(:hard_trainer) { HardTrainer.new(dealer_card: 3) }
+
+    player_hit_hands = (4..8).to_a + [12]
+    player_hit_hands.each do |hand|
+      it "returns hit if player has #{hand}" do
+        expect(hard_trainer.call(player_total: hand)).to eq(:hit)
+      end
+    end
+
+    player_double_hands = [9,10,11]
+    player_double_hands.each do |hand|
+      it "returns double if player has #{hand}" do
+        expect(hard_trainer.call(player_total: hand)).to eq(:double)
+      end
+    end
+
+    player_stand_hands = (13..20)
+    player_stand_hands.each do |hand|
+      it "returns stand if player has #{hand}" do
+        expect(hard_trainer.call(player_total: hand)).to eq(:stand)
+      end
+    end
+  end
 end
