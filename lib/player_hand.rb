5d72d84cfe8399a6e96763f59347c6dcd99aeacd
Toby Muir-Little <tjcmui@gmail.com>
2023-06-08 09:42:23 +0100

    handle multiple As

--- lib/player_hand.rb
+++ lib/player_hand.rb
@@ -6,20 +6,39 @@ class PlayerHand
   end
 
   def value
-    cards.inject(0) {|sum, card| sum += convert_card_to_value(card)}
+    return 2 if cards == ['A', 'A']
+
+    value = cards.inject(0) {|sum, card| sum += convert_card_to_value(card)}
+    return value unless cards.include?('A') && value > 21
+
+    ace_count = cards.count('A')
+
+    until value <= 21 || ace_count == 0
+      value -= 10
+      ace_count -= 1
+    end
+
+    value
   end
 
   def type
     return :split if hand_split?
-    return :soft if cards.any?{ card_is_ace?(_1) }
+    return :soft if cards.any?{ card_is_ace?(_1) } && soft_hand(cards)
 
     :hard
   end
 
   private
 
+  def soft_hand(cards)
+    change_ace_to_one(cards).inject(0) {|sum, card| sum += convert_card_to_value(card)} < 12
+  end
+
+  def change_ace_to_one(cards)
+    cards.map{|card| card == 'A' ? 1 : card}
+  end
+
   def convert_card_to_value(card)
-    return 1 if card_is_ace?(card) && hand_split?
     return 11 if card_is_ace?(card)
     return 10 if face_card?(card)
 
@@ -35,6 +54,6 @@ class PlayerHand
   end
 
   def hand_split?
-    cards[0] == cards[1]
+    cards[0] == cards[1] && cards.length == 2
   end
 end
--- spec/player_hand_spec.rb
+++ spec/player_hand_spec.rb
@@ -34,6 +34,46 @@ RSpec.describe PlayerHand do
     it 'handles K as 10' do
       expect(player_hand.new(cards: ['K', 2]).value).to eq(12)
     end
+
+    it 'handles A, 10 as 21' do
+      expect(player_hand.new(cards: ['A', 10]).value).to eq(21)
+    end
+
+    it 'handles A, 5, 8 as 14' do
+      expect(player_hand.new(cards: ['A', 5, 8]).value).to eq(14)
+    end
+
+    it 'handles 4, 9, A as 14' do
+      expect(player_hand.new(cards: [4, 9, 'A']).value).to eq(14)
+    end
+
+    it 'handles 4, 9, A as 15' do
+      expect(player_hand.new(cards: [4, 9, 'A', 'A']).value).to eq(15)
+    end
+
+    it 'handles A, A, A as 13' do
+      expect(player_hand.new(cards: ['A', 'A', 'A']).value).to eq(13)
+    end
+
+    it 'handles A, A, A, A as 14' do
+      expect(player_hand.new(cards: ['A', 'A', 'A', 'A']).value).to eq(14)
+    end
+
+    it 'handles A, 2, A, A as 16' do
+      expect(player_hand.new(cards: ['A', 2, 'A', 'A', 'A']).value).to eq(16)
+    end
+
+    it 'handles A, 2, A, A, 10 as 15' do
+      expect(player_hand.new(cards: ['A', 2, 'A', 'A', 10]).value).to eq(15)
+    end
+
+    it 'handles A, 2, A, 8, 10 as 22' do
+      expect(player_hand.new(cards: ['A', 2, 'A', 8, 10]).value).to eq(22)
+    end
+
+    it 'handles A, A, 8 as 20' do
+      expect(player_hand.new(cards: ['A', 'A', 8]).value).to eq(20)
+    end
   end
 
   describe '.type' do
@@ -51,6 +91,46 @@ RSpec.describe PlayerHand do
 
     it 'returns the type of hand as split if the same cards included' do
       expect(player_hand.new(cards: ['A', 'A']).type).to eq(:split)
+    end
+
+    it 'handles A, 10 as soft' do
+      expect(player_hand.new(cards: ['A', 10]).type).to eq(:soft)
+    end
+
+    it 'handles A, 5, 8 as hard' do
+      expect(player_hand.new(cards: ['A', 5, 8]).type).to eq(:hard)
+    end
+
+    it 'handles 4, 9, A as hard' do
+      expect(player_hand.new(cards: [4, 9, 'A']).type).to eq(:hard)
+    end
+
+    it 'handles 4, 9, A as hard' do
+      expect(player_hand.new(cards: [4, 9, 'A', 'A']).type).to eq(:hard)
+    end
+
+    it 'handles A, A, A as soft' do
+      expect(player_hand.new(cards: ['A', 'A', 'A']).type).to eq(:soft)
+    end
+
+    it 'handles A, A, A, A as soft' do
+      expect(player_hand.new(cards: ['A', 'A', 'A', 'A']).type).to eq(:soft)
+    end
+
+    it 'handles A, 2, A, A as soft' do
+      expect(player_hand.new(cards: ['A', 2, 'A', 'A', 'A']).type).to eq(:soft)
+    end
+
+    it 'handles A, 2, A, A, 10 as hard' do
+      expect(player_hand.new(cards: ['A', 2, 'A', 'A', 10]).type).to eq(:hard)
+    end
+
+    it 'handles A, 2, A, 8, 10 as hard' do
+      expect(player_hand.new(cards: ['A', 2, 'A', 8, 10]).type).to eq(:hard)
+    end
+
+    it 'handles A, A, 8 as soft' do
+      expect(player_hand.new(cards: ['A', 'A', 8]).type).to eq(:soft)
     end
   end
 end
