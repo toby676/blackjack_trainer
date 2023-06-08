df6854b4f158edcc439dc517d0dbc531d13698ea
Toby Muir-Little <tjcmui@gmail.com>
2023-06-08 10:39:15 +0100

    multiple turns before bust

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -18,24 +18,48 @@ class PlayGame
 
       player_cards = [player_first_card, player_second_card]
 
-      current_move = ResolveMove.new.call(dealer_card:, player_cards: player_cards).to_s
+      current_move = resolve_move(dealer_card:, player_cards:)
 
       puts "Dealer has: #{dealer_card}"
-      puts "You have: #{player_first_card}, #{player_second_card}"
+      puts "You have: #{player_cards}"
       puts "What do you do?"
 
-      try = ''
-
-      while try != current_move
+      while current_move != :bust
         try = gets.chomp
-        if try == current_move
-          puts "Correct!"
+        if try == current_move.to_s && current_move == :stand
+          puts "Correct! - You have picked stand and the round is over!"
           puts "========="
+          break
+        elsif try == current_move.to_s && current_move == :split
+          puts "Correct! - You have picked split and the round is over!"
+          puts "========="
+          break
+        elsif try == current_move.to_s && current_move == :double
+          puts "Correct! - You have picked double and the round is over!"
+          puts "========="
+          break
+        elsif try == current_move.to_s && current_move == :hit
+          puts "Correct! - You have hit and the next card is drawn!"
+          player_cards << RandomCardGenerator.call
+          current_move = resolve_move(dealer_card:, player_cards:)
+          puts "Dealer has: #{dealer_card}"
+          puts "You have: #{player_cards}"
+          if current_move == :bust
+            puts "Oh No! You're bust!"
+            puts "========="
+          else
+            puts "What do you do?"
+          end
+          next
         else
           puts "Try again."
         end
       end
     end
+  end
+
+  def resolve_move(dealer_card:, player_cards:)
+    ResolveMove.new.call(dealer_card:, player_cards: player_cards)
   end
 end
 
