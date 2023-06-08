cd36ea8f6154b61e847277ff3e3fec1e6ea0d8e1
Toby Muir-Little <tjcmui@gmail.com>
2023-06-08 10:23:03 +0100

    better game with turns

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,32 +1,42 @@
 require_relative 'random_card_generator'
 require_relative 'resolve_move'
 
-puts "Welcome to the black jack trainer!"
-puts "Please enter one of 'stand', 'hit', 'double', 'split' depending on the game situation"
-puts "You can keep trying until you are correct"
-puts "Type exit at any point to leave"
+class PlayGame
+  def call
+    puts "Welcome to the black jack trainer!"
+    puts "Please enter one of 'stand', 'hit', 'double', 'split' depending on the game situation"
+    puts "You can keep trying until you are correct"
+    puts "How many times would you like to play?"
 
-dealer_card = RandomCardGenerator.call
-player_first_card = RandomCardGenerator.call
-player_second_card = RandomCardGenerator.call
+    plays = gets.chomp.to_i
 
-correct_move = ResolveMove.new.call(dealer_card:, player_cards: [player_first_card, player_second_card]).to_s
+    plays.times do |i|
+      puts "Turn #{i + 1}"
+      dealer_card = RandomCardGenerator.call
+      player_first_card = RandomCardGenerator.call
+      player_second_card = RandomCardGenerator.call
 
-puts "Dealer has: #{dealer_card}"
-puts "You have: #{player_first_card}, #{player_second_card}"
-puts "What do you do?"
+      player_cards = [player_first_card, player_second_card]
 
-try = gets.chomp
+      current_move = ResolveMove.new.call(dealer_card:, player_cards: player_cards).to_s
 
-while try != correct_move
-  puts "Try again."
-  try = gets.chomp
-  if try == 'exit'
-    puts "Okay bye!"
-    break
-  end
-  if try == correct_move
-    puts "You win!"
-    break
+      puts "Dealer has: #{dealer_card}"
+      puts "You have: #{player_first_card}, #{player_second_card}"
+      puts "What do you do?"
+
+      try = ''
+
+      while try != current_move
+        try = gets.chomp
+        if try == current_move
+          puts "Correct!"
+          puts "========="
+        else
+          puts "Try again."
+        end
+      end
+    end
   end
 end
+
+PlayGame.new.call
